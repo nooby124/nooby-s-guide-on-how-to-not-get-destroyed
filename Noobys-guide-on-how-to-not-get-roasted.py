4494e69e6582d9727847e462a11e54a42d08bddd
nooby124 <139121928+nooby124@users.noreply.github.com>
2023-07-14 12:15:21 +0300

    Add files via upload

---
+++ Noobys-guide-on-how-to-not-get-roasted.py
@@ -0,0 +1,23 @@
+import streamlit as st
+from PIL import Image
+
+hide_st_style = """
+            <style>
+            #MainMenu {visibility: hidden;}
+            footer {visibility: hidden;}
+            header {visibility: hidden;}
+            </style>
+            """
+st.markdown(hide_st_style, unsafe_allow_html=True)
+
+st.title('A simple guide on how to not get roasted')
+st.write('1. dont send this message in the class group chat:')
+image = Image.open('ha.png')
+
+st.image(image)
+
+st.title('thats it!')
+st.write('---')
+st.write("[anyways view the source code here! >](https://github.com/nooby124/nooby-s-guide-on-how-to-not-get-destroyed)")
+st.write('---')
+st.write('this website is just a joke for anyone who is not from my class but seriously dont say that in any group chat. for the guys that are in my class: this is very serious please take these things very personally. (looking at u mr a.a).')
